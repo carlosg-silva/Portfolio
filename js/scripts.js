2940d14fef27b9c3e0a276477e0324b2da6b9d59
Carlos <cgomessilva@gmail.com>
2021-05-05 23:49:57 -0300

    Modificando variavel

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ function abrirNav() {
 
     var x = document.getElementById("navigation");
 
-    if (x.className === "navigation") {
-       x.className += " menujs";
+    if (botao.className === "navigation") {
+       botao.className += " menujs";
        document.getElementById("botaoMenu").innerHTML = "&Cross;";
     } else {
-       x.className = "navigation";
+       botao.className = "navigation";
        document.getElementById("botaoMenu").innerHTML = "&#9776;";	
     }
 
